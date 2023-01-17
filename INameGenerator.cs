e08891ed9d5f67168a08224f20de9202912bba0e
beavisulochka <102587784+beavisulochka@users.noreply.github.com>
2023-01-17 19:35:22 +0100

    Add files via upload

---
+++ INameGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NameGenerator
+{
+	public interface INameGenerator
+	{
+        string NickGenerator();
+		char FirstLetterChoise();
+		char SecondLetterChoise(char firstLetter);
+		string OtherLettersCreate();
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NameGenerator
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			NameGenerator Test = new NameGenerator();
+			Test.NickGenerator();
+		}
+	}
+}
+
